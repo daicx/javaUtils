415c9c70029108c9fe5f292577f2a453bbd7a0da
daichangxia <daichangxia@58.com>
2020-10-30 19:48:37 +0800

    线程池

--- src/main/java/com/lenovo/javautils/utils/MyThreadPoolExecutor.java
+++ src/main/java/com/lenovo/javautils/utils/MyThreadPoolExecutor.java
@@ -59,4 +59,13 @@ public enum MyThreadPoolExecutor {
             }
         }
     }
+    private class CustomRejectedExecutionHandler1 implements RejectedExecutionHandler {
+
+        @Override
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+            // 记录异常
+            // 报警处理等
+            System.out.println("error.............");
+        }
+    }
 }
